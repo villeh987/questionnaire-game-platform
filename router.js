c65319e4191b22cb0f2da2b5ab9db74996dcc0fe
Vilhelmi Lehtonen <vilhelmi.lehtonen@tuni.fi>
2019-12-07 16:41:10 +0000

    Started to implement management view. Added views, routes and controllers for questionnaire.

--- router.js
+++ router.js
@@ -3,9 +3,11 @@
 // load routers
 const UsersRouter = require('./routes/users');
 const HelloRouter = require('./routes/hello');
+const QuestionnairesRouter = require ('./routes/questionnaire');
 
 // Setup Routes
 module.exports = function(app) {
     app.use('/users', UsersRouter);
     app.use('/', HelloRouter);
+    app.use('/questionnaires', QuestionnairesRouter);
 };
