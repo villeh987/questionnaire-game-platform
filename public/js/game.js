dcff70112d65eb5daf95467e65bf1f34187eb9ae
Mika <placeholder@notreal.email>
2019-12-12 22:06:21 +0200

    Collision detection between player and options. Moved stuff around ( creating tests in the beginning now )

--- public/js/game.js
+++ public/js/game.js
@@ -1,6 +1,72 @@
 'use strict';
 
-//var Phaser = require('phaser');
+//TODO: Load questionnaire from database
+
+/* TEST STUFF:
+/  generate list of random string questionnaires
+*/
+
+const NUM_TESTQUESTIONS = 3;
+
+const MIN_TITLELENGTH = 1;
+const MAX_TITLELENGTH = 100;
+const MIN_OPTIONS = 2;
+const MAX_OPTIONS = 7;
+const MIN_POINTS = 1;
+
+const MIN_OPTIONLENGTH = 1;
+const MAX_OPTIONLENGTH = 50;
+
+//Generate random number between min and max
+function randomIntBetween(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+//Generates random string from set of characters
+function generateRandomString(min, max) {
+    let string = "";
+    const SELECTION = "ABCDEFGHIJKLMNOPQRSTUVWXYZOÄÖabcdefghijklmnopqrstuvwxyzoäö01234567890";
+
+    let numChars = randomIntBetween(min, max);
+
+    for(let i = 0; i < numChars; ++i) {
+        string += SELECTION.charAt(Math.floor(Math.random() * SELECTION.length));
+    }
+
+    return string;
+}
+
+//Generates list of random test options
+function generateOptions(min, max) {
+    let options = [];
+
+    let numOptions = randomIntBetween(min, max);
+
+    for(let i = 0; i < numOptions; ++i) {
+        options.push(
+            {
+                title: generateRandomString(MIN_OPTIONLENGTH, MAX_OPTIONLENGTH),
+                correctness: false
+            }
+        );
+    }
+
+    //set one option as the correct one
+    options[randomIntBetween(0, options.length -1)]['correctness'] = true;
+
+    return options;
+}
+
+let questionnaire = [];
+for(let i = 0; i < NUM_TESTQUESTIONS; ++i) {
+    questionnaire.push(
+        {
+            title: generateRandomString(MIN_TITLELENGTH, MAX_TITLELENGTH),
+            options: generateOptions(MIN_OPTIONS, MAX_OPTIONS)
+        }
+    );
+}
+//END of test stuff
 
 var config = {
     type: Phaser.AUTO,
@@ -24,10 +90,15 @@ var config = {
             crosses: null,
             time: 0,
             topMargin: 100,
-            magicTopMarginNumber: 48
+            magicTopMarginNumber: 48,
         }
     }
 };
+
+let questionNumber = 0;
+let optionNumber = 0;
+
+let game = new Phaser.Game(config);
 
 let Bullet = new Phaser.Class({
 
@@ -71,18 +142,42 @@ let Bullet = new Phaser.Class({
     }
 });
 
-let Option = new Phaser.class({
+let Option = new Phaser.Class({
 
     Extends: Phaser.GameObjects.Text,
 
     initialize:
 
-    function option()
+    function Option(scene) {
+        Phaser.GameObjects.Text.call(
+            this,
+            scene,
+            0,
+            0,
+            questionnaire[questionNumber].options[optionNumber].title,
+            {
+                font: 'bold 18px Arial',
+                fill: 'white',
+                wordWrap: { width: 100, useAdvancedWrap: true }
+            });
+            this.scene.physics.world.enable(this);
+            //TODO: random speed
+            this.body.setVelocity(29);
+            this.setActive(true);
+            this.setPosition(400, 200);
+    },
 
-}
+    update: function(time, delta) {
+        //TODO: increase speed in random dir
+        this.body.velocity.x += 0.001;
+    },
 
-
-let game = new Phaser.Game(config);
+    destroy: function() {
+        this.setActive(false);
+        this.setVisible(false);
+        this.body.setEnable(false);
+    }
+});
 
 function preload () {
     this.load.spritesheet('ship', '../img/pixel_ship.png',
@@ -92,81 +187,8 @@ function preload () {
         { frameWidth: 16, frameHeight: 16 }
     );
 
-    //TODO: Load questionnaire from database
-
-    /* TEST STUFF:
-    /  generate list of random string questionnaires
-    */
-
-    const NUM_TESTQUESTIONS = 3;
-
-    const MIN_TITLELENGTH = 1;
-    const MAX_TITLELENGTH = 100;
-    const MIN_OPTIONS = 2;
-    const MAX_OPTIONS = 7;
-    const MIN_POINTS = 1;
-
-    const MIN_OPTIONLENGTH = 1;
-    const MAX_OPTIONLENGTH = 50;
-
-    //Generate random number between min and max
-    function randomIntBetween(min, max) {
-        return Math.floor(Math.random() * (max - min + 1) + min);
-    }
-
-    //Generates random string from set of characters
-    function generateRandomString(min, max) {
-        let string = "";
-        const SELECTION = "ABCDEFGHIJKLMNOPQRSTUVWXYZOÄÖabcdefghijklmnopqrstuvwxyzoäö01234567890";
-
-        let numChars = randomIntBetween(min, max);
-
-        for(let i = 0; i < numChars; ++i) {
-            string += SELECTION.charAt(Math.floor(Math.random() * SELECTION.length));
-        }
-
-        return string;
-    }
-
-    //Generates list of random test options
-    function generateOptions(min, max) {
-        let options = [];
-
-        let numOptions = randomIntBetween(min, max);
-
-        for(let i = 0; i < numOptions; ++i) {
-            options.push(
-                {
-                    title: generateRandomString(MIN_OPTIONLENGTH, MAX_OPTIONLENGTH),
-                    correctness: false
-                }
-            );
-        }
-
-        //set one option as the correct one
-        options[randomIntBetween(0, options.length -1)]['correctness'] = true;
-
-        return options;
-    }
-
-    let questionnaire = [];
-    for(let i = 0; i < NUM_TESTQUESTIONS; ++i) {
-        questionnaire.push(
-            {
-                title: generateRandomString(MIN_TITLELENGTH, MAX_TITLELENGTH),
-                options: generateOptions(MIN_OPTIONS, MAX_OPTIONS)
-            }
-        );
-    }
-    //END of test stuff
-}
-
-//Spawns one option from questionnaire on the playfield
-function spawnOption() {
 
 }
-
-
 
 function create () {
     //Create groups
@@ -179,8 +201,10 @@ function create () {
     this.crosses = crosses;
 
     let options = this.physics.add.group({
+        classType: Option,
         defaultKey: 'option',
-        maxSize: 3
+        maxSize: 3,
+        runChildUpdate: true
     });
     this.options = options;
 
@@ -208,24 +232,31 @@ function create () {
         }
     });
 
-    //test putting text box in there
-    let testOption = this.make.text({
-        x: 200,
-        y: 300,
-        text: 'answeransweransl aölskdjfa ölskdjf aölskdjf ö',
-        origin: { x: 0, y: 0 },
-        style: {
-            font: 'bold 18px Arial',
-            fill: 'white',
-            wordWrap: { width: 100, useAdvancedWrap: true }
-        }
-    });
 
-    this.physics.world.enable(testOption);
-    testOption.body.setVelocity(29);
+    let testOption = options.get();
+
     question.text = "Question";
 
     //END test stuff
+
+    //collision detection between groups with callback
+    this.physics.add.collider(
+        options,
+        this.player,
+        function (player, option) {
+            console.log(option);
+            console.log(player);
+            if(questionnaire[questionNumber].options[optionNumber].correct) {
+                console.log("collected correct one");
+                //TODO add point
+            } else {
+                console.log("wrong one");
+                console.log(option);
+                //TODO what happens here?
+            }
+            option.destroy();
+        }
+    );
 
 
     //Input config
