0b30f8629c88fccdb14ef928c9200406942384cb
Vilhelmi Lehtonen <vilhelmi.lehtonen@tuni.fi>
2019-12-20 10:21:28 +0000

    Cleaned and added documentation to createQuestionnaire.js

--- public/js/createQuestionnaire.js
+++ public/js/createQuestionnaire.js
@@ -1,73 +1,79 @@
 'use strict';
 
-// This file contains javascript
-
-$("#javascript_start").html("[OK] Started executing the javascript file");
-$("#javascript_end").html("[WAITING]...this far we haven't reached the end... Maybe you should check dev tools?");
+// This file contains javascript needed to dynamically add elements to questionnaire forms.
 
 var question_number = 2;
 var option_number = 3;
 var parent_question_number = 1;
 
+/**
+ * Increments question number according to amount of questions.
+ */
 function incrementQuestionNumber() {
 
-	//let question_id =  
 	question_number = $("#questions").children().length + 1;
-
-
-	//console.log( $("#questions").children().last().attr("id") );
-	//console.log( question_number );
-	//question_number += 1;
 }
 
-function getParentQuestionNumber(element) {
 
-	//parent_question_number = $("#questions").children().length;
+/**
+ * Sets parent question number when adding
+ * new option to questionnaire form.
+ * @param {element} element which sends the signal.
+ */
+function getParentQuestionNumber(element) {
 
     let parent_id = $(element).parent().attr("id");
     parent_question_number = parent_id.replace(/question/, '');
-
-	/*
-	let parent_question_id = element.parentElement.id;
-	parent_question_number = parent_question_id.slice(parent_question_id.length - 1); */
 }
 
+/**
+ * Finds the proper new option number when adding
+ * new options to questionnaire form.
+ * @param {element} element which sends the signal.
+ */
 function getOptionNumber(element) {
 
 	let counter = 0;
-	$(element).parent().children().each( (i, child) =>{
+	$(element).parent().children().each((i, child) =>{
 		if ($(child).is("div") && $(child).find("label").first().text() !== "Max Points") {
 			counter += 1;
 		}
-	} )
-	//let last_option_id = $(element).prev().attr("id");
-	//option_number = Number(last_option_id.slice(last_option_id.length - 1)) + 1;
+	});
 	option_number = counter + 1;
 }
 
-
+/**
+ * Finds the proper new option number when removing
+ * options from questionnaire form.
+ * @param {element} element which sends the signal.
+ */
 function getNewOptionNumberOnRemove(element) {
 	let parent_question = $(element).parent();
 
 	let counter = 0;
 
-	$(parent_question).children().each( (i, child) =>{
+	$(parent_question).children().each((i, child) => {
 		if ($(child).is("div") && $(child).find("label").first().text() !== "Max Points") {
 			counter += 1;
 			if (element == child) {
 				return false;
 			}
 		}
-	} )
+	});
 
 	return counter - 1;
 }
 
+/**
+ * Finds the proper new option number when removing
+ * questions from questionnaire form.
+ * @param {element} element which sends the signal.
+ */
 function getNewQuestionNumberOnRemove(element) {
 
     let questions = $(element).parent();
     let counter = 0;
-    $(questions).children().each( (i, child) =>{
+    $(questions).children().each((i, child) =>{
         if ($(child).is("div")) {
             counter += 1;
             
@@ -75,18 +81,22 @@ function getNewQuestionNumberOnRemove(element) {
                 return false;
             }
         }
-    } )
+    });
 
     return counter - 1;
 }
 
 
-
+/**
+ * Adds new question to the questionnaire form
+ * dynamically, when 'Add question' button is
+ * clicked.
+ */
 function addNewQuestion() {
 
 
 	incrementQuestionNumber();
-	const add = `<div class="form-group" id="question${question_number}">
+	const newQuestion = `<div class="form-group" id="question${question_number}">
 				    <label for="questionInput${question_number}"><b>Question ${question_number}</b></label><span> </span><button type="button" class="btn btn-primary" onclick="removeQuestion(this)"><i class="fas fa-trash-alt"></i></button>
 			  		<input type="text" class="form-control" aria-label="Text input with checkbox" placeholder="Enter question" name="questions[${question_number}][title]" required="true">  
 
@@ -129,15 +139,20 @@ function addNewQuestion() {
 				</div>`
 
 
-	$("#questions").append(add);
+	$("#questions").append(newQuestion);
 
 };
 
+/**
+ * Adds new option to the questionnaire form
+ * dynamically, when 'Add question' button is
+ * clicked.
+ */
 function addNewOption(element) {
 	getParentQuestionNumber(element);
 	getOptionNumber(element);
 
-	const add2 = `
+	const newOption = `
 			<div class="form-group ml-5" id="option${option_number}">
 				<label for="optionInput${option_number}">Option ${option_number}</label>
 				<div class="input-group mb-3">
@@ -157,36 +172,38 @@ function addNewOption(element) {
 
 
 
-	$(element).after(add2);
+	$(element).after(newOption);
 	element.remove();
 
 };
 
+/**
+ * Removes question from the questionnaire form
+ * dynamically, when 'Remove question' button is
+ * clicked.
+ */
 function removeQuestion(element) {
 
 	
 	$(element).parent().nextAll().each( (i, el) => {
-		//console.log(el);
-		let question_id_number = getNewQuestionNumberOnRemove(el);  // TODO: laske optionit
-		//question_id_number -= 1;
+
+		let question_id_number = getNewQuestionNumberOnRemove(el);
+
 		$(el).attr("id", `question${question_id_number}`)
 		$(el).children().find("b").text(`Question ${question_id_number}`);
 		$(el).find("label").first().attr("for", `questionInput${question_id_number}`);
 		$(el).find("input").first().attr("name", `questions[${question_id_number}][title]`);
-		//$(el).find("input").first().attr("id", `questionInput${question_id_number}`);
 		$(el).find("div").first().find("input").attr("name", `questions[${question_id_number}][maxPoints]`);
 
 		$(el).children().each( (i, option) => {
-			//console.log("optio", option);
+
 			if ($(option).is("div") && $(option).find("label").first().text() !== "Max Points") {
-				
 
 				// Option Name
 
 				let option_name = $(option).find("div").first().find("input").first().attr("name");
                 option_name = option_name.replace(/questions\[\d*\]/, `questions[${question_id_number}]`);
 				$(option).find("div").first().find("input").first().attr("name", option_name);
-
 
 				// Correctness name
 
@@ -200,8 +217,7 @@ function removeQuestion(element) {
 				hint_name = hint_name.replace(/questions\[\d*\]/, `questions[${question_id_number}]`);
 				$(option).find($(".form-control.option-hint")).attr("name", hint_name);
 			}
-		})
-
+		});
 
 	}); 
 
@@ -210,9 +226,12 @@ function removeQuestion(element) {
 
 }
 
+/**
+ * Removes option from the questionnaire form
+ * dynamically, when 'Remove question' button is
+ * clicked.
+ */
 function removeOption (element) {
-
-	
 
 	let following_options = $(element).parent().parent().parent().nextAll();
 	following_options.each( (i, option) => {
@@ -220,11 +239,9 @@ function removeOption (element) {
 
 
 			let new_option_number = getNewOptionNumberOnRemove(option);
-            //console.log(new_option_number);
 			$(option).attr("id", `option${new_option_number}`);
 			$(option).find("label").first().text(`Option ${new_option_number}`);
 			$(option).find("label").first().attr("for", `optionInput${new_option_number}`);
-			//$(option).find("input").first().attr("id", `optionInput${new_option_number}`);
 
 			// Option Name
 
@@ -244,9 +261,7 @@ function removeOption (element) {
 			hint_name = hint_name.replace(/\[\d*\]\[hint\]/, `[${new_option_number}][hint]`)
 			$(option).find($(".form-control.option-hint")).attr("name", hint_name);
 		}
-	})
+	});
 
 	element.parentElement.parentElement.parentElement.remove();
 }
-
-$("#javascript_end").html("[OK] The end of your javascript file was reached. (meaning there were no huge errors) Hopefully your code works too! ");
