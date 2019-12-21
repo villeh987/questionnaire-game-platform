91609a0abb4e34c026509be116df8f180ebad3dc
Aleksi Raatala <aleksi.raatala@tuni.fi>
2019-12-21 22:04:04 +0200

    Added unit tests for game routes, controller and grader model

---
+++ test/assignment/game_view.test.js
@@ -0,0 +1,186 @@
+/* eslint-disable no-invalid-this */
+'use strict';
+
+require('dotenv').config();
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const config = require('config');
+//const mongoose = require('mongoose');
+const fs = require('fs');
+const path = require('path');
+const app = require('../../app');
+const User = require('../../models/user');
+const Questionnaire = require('../../models/questionnaire');
+const Grader = require('../../models/grader');
+const Ranking = require('../../models/ranking');
+
+const DomParser = require('dom-parser');
+const parser = new DomParser();
+
+
+const expect = chai.expect;
+const should = chai.should();
+chai.use(chaiHttp);
+
+const admin = config.get('admin');
+const loginUrl = '/users/login';
+const listURL = '/games';
+
+
+function parseCsrfToken(res) {
+    let htmlDoc = parser.parseFromString(res.text, 'text/html');
+    return htmlDoc.getElementsByName('csrf-token')[0].getAttribute('content');
+}
+
+
+describe('Game view', function() {
+
+    let request;
+
+    let testQuestionnaire;
+
+    let data;
+
+    let csrfToken;
+
+    after(function(done) {
+        //mongoose.disconnect(done);
+        done();
+    });
+
+    this.beforeAll( async function() {
+        request = chai.request.agent(app);
+
+
+        // Create admin user, if doesn't exist
+        const userData = {...admin, role: 'admin'};
+        const user = new User(userData);
+        try {
+            await user.save();
+        } catch(error) {
+            // Admin exists already in db.
+        }
+
+        // Empty database
+        await Questionnaire.deleteMany({});
+
+
+        // Create test data
+        let rawData = fs.readFileSync( path.resolve(__dirname, './test_data.json') );
+        data =  JSON.parse(rawData);
+        await Questionnaire.create(data);
+
+        // Get test questionnaire
+        testQuestionnaire = await Questionnaire.findOne({title : 'Test questionnaire'}).exec();
+
+
+        //done();
+    });
+
+    this.afterAll(function(done) {
+        request.close(done);
+    });
+
+    it('Login as Admin', async function() {
+        const response = await request
+            .post(loginUrl)
+            .type('form')
+            .send(admin);
+        expect(response).to.have.cookie('bwa');
+        expect(response).to.redirectTo(/\/$/);
+    });
+
+    it('Should list all questionnaires as games', async function() {
+        const response = await request
+            .get(listURL);
+
+        expect(response.statusCode).to.equal(200);
+    });
+
+
+    it('Should load a game view', async function() {
+
+        const response = await request
+            .get(`${listURL}/${testQuestionnaire.id}`)
+
+        expect(response.statusCode).to.equal(200);
+    });
+
+    it('Should grade the game', async function() {
+        let gameForm = fs.readFileSync( path.resolve(__dirname, './game_test_form.json') );
+        gameForm = JSON.parse(gameForm);
+
+        const response = await request
+            .post(`${listURL}/${testQuestionnaire.id}`)
+            .type('form')
+            .send(gameForm);
+
+        expect(response.statusCode).to.equal(200);
+        response.should.have.property('text');
+        response.text.should.not.have.lengthOf(0);
+    });
+
+});
+
+describe('Grader', function() {
+    let testQuestionnaire;
+    let data;
+    const gameData = {
+        points: 10,
+        errors: 0,
+        maxPoints: 100
+    }
+
+    after(function(done) {
+        done();
+    });
+
+    this.beforeAll( async function() {
+
+        // Empty ranking database
+        await Ranking.deleteMany({});
+
+        // Empty questionnaire database
+        await Questionnaire.deleteMany({});
+
+
+        // Create test data
+        let rawData = fs.readFileSync( path.resolve(__dirname, './test_data.json') );
+        data =  JSON.parse(rawData);
+        await Questionnaire.create(data);
+
+        // Get test questionnaire
+        testQuestionnaire = await Questionnaire.findOne({title : 'Test questionnaire'}).exec();
+
+    });
+
+    it('Should grade the game and return a score', async function() {
+        let score = await Grader.grade(gameData.points, gameData.errors,
+                      gameData.maxPoints, testQuestionnaire.id, "Test user");
+        expect(score).to.equal(100);
+
+    });
+
+    it('Should save the game data to Ranking database', async function() {
+      let score = await Grader.grade(gameData.points, gameData.errors,
+                    gameData.maxPoints, testQuestionnaire.id, "Test user");
+      let rankingList = await Ranking.find().exec();
+      let leaderboard = rankingList[0].gameScore;
+
+      expect(rankingList).to.have.lengthOf(1);
+      expect(leaderboard[0].player).to.equal('Test user');
+      expect(leaderboard[0].grade).to.equal(100);
+    });
+
+    it('should only save 10 entries for each game', async function() {
+        for(let i = 11; i > 0; --i) {
+            await Grader.grade(gameData.points, i, gameData.maxPoints,
+                               "testID", "Test user" + i);
+        }
+        let rankingList = await Ranking.findOne({game: "testID"}).exec();
+        let leaderboard = rankingList.gameScore;
+        expect(leaderboard).to.have.lengthOf(10);
+        expect(leaderboard.slice(-1).pop().player).to.equal('Test user10');
+
+    });
+});
---
+++ test/assignment/game_view_ui.test.js
@@ -0,0 +1,91 @@
+/* eslint-disable no-console */
+'use strict';
+
+require('dotenv').config();
+//const assert = require('assert');
+const config = require('config');
+const http = require('http');
+const Browser = require('zombie');
+
+const fs = require('fs');
+const path = require('path');
+
+const app = require('../../app.js');
+const admin = config.get('admin');
+const Questionnaire = require('../../models/questionnaire');
+const port = 3333;
+
+async function auth(browser) {
+    // Load login page
+    await browser.visit('/users/login');
+
+    // Fill in login information and login
+    browser.fill('email', admin.email);
+    browser.fill('password', admin.password);
+    await browser.pressButton('#btnLogin');
+}
+
+describe('Game view UI test suite', function() {
+    let server;
+    let browser;
+
+    let data;
+    let testQuestionnaires;
+
+    beforeEach(async function() {
+
+        // Empty database
+        await Questionnaire.deleteMany({});
+
+
+        // Create test data
+        let rawData = fs.readFileSync( path.resolve(__dirname, './ui_test_data.json') );
+        data =  JSON.parse(rawData);
+        await Questionnaire.create(data);
+
+        // Get test questionnaire
+        testQuestionnaires = await Questionnaire.find().exec();
+
+
+        server = http.createServer(app).listen(port);
+        Browser.localhost('bwa', port);
+        browser = new Browser();
+
+        await auth(browser);
+        //await browser.visit('/questionnaires');
+        //await browser.pressButton('#grade');
+    });
+
+    afterEach(function() {
+        server.close();
+    });
+
+    describe('/games', function() {
+
+        beforeEach(async function() {
+            await browser.visit('/games');
+        });
+
+        it('should be successful', function() {
+            browser.assert.success();
+        });
+
+        it('must be able to view all questionnaires as games', function() {
+
+            for(let questionnaire in testQuestionnaires) {
+                browser.assert.text('title', questionnaire.title);
+            }
+        });
+
+        it('should have play-button', function() {
+            browser.assert.element(`#button_${testQuestionnaires[0].id}`);
+        });
+
+        it('play-button should route to games/:id', function() {
+            browser.assert.attribute(`#button_${testQuestionnaires[0].id}`, 'href',
+                `/games/${testQuestionnaires[0].id}`);
+        });
+
+    });
+
+});
