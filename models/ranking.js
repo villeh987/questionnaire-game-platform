16fc3ed0e4c624f704d6e65f5ceed3db5150f19c
Aleksi Raatala <aleksi.raatala@tuni.fi>
2019-12-22 20:39:21 +0200

    Deleted an unnesessary validation

--- models/ranking.js
+++ models/ranking.js
@@ -37,24 +37,7 @@ const rankingSchema = new Schema({
             validator: (val) => {
                 if (!val || !Array.isArray(val)) return false;
                 if (val.length === 0) return false;
-
-                const uniqueGamescores = [];
-                val.forEach((current) => {
-                    if (!current.player) return;
-                    const player = current.player.trim().toUpperCase();
-
-                    if (uniqueGamescores.includes(player)) {
-                        uniqueGamescores[player].rights = current.rights;
-                        uniqueGamescores[player].wrongs = current.wrongs;
-                        uniqueGamescores[player].grade = current.grade;
-                        return;
-                    }
-                    uniqueGamescores.push(player);
-                });
-
-                return uniqueGamescores.length === val.length;
             },
-            message: 'Questionnaire must have at least one question and question titles must be unique'
         }
     }
 });
